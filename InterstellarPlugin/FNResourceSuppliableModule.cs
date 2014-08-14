759a1c27e3920b79c1067a1851c58f4634e75c44
WaveFunctionP <m.corey.thompson@gmail.com>
2014-08-13 19:45:06 -0500

    - more part changes - rename project to match mod name - reorganization work

---
+++ InterstellarPlugin/FNResourceSuppliableModule.cs
@@ -0,0 +1,19 @@
+﻿extern alias ORSv1_1;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ORSv1_1::OpenResourceSystem;
+
+namespace InterstellarPlugin {
+    abstract class FNResourceSuppliableModule : ORSResourceSuppliableModule{
+        protected override ORSResourceManager createResourceManagerForResource(string resourcename) {
+            return getOvermanagerForResource(resourcename).createManagerForVessel(this);
+        }
+
+        protected override ORSResourceOvermanager getOvermanagerForResource(string resourcename) {
+            return FNResourceOvermanager.getResourceOvermanagerForResource(resourcename);
+        }
+    }
+}
---
+++ InterstellarPlugin/FNThermalSource.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace InterstellarPlugin{
+	public interface FNThermalSource{
+
+		float getCoreTemp();
+
+		float getThermalPower();
+
+        float getChargedPower();
+
+		bool getIsNuclear();
+
+		float getRadius();
+
+		bool isActive();
+
+		void enableIfPossible();
+
+        bool shouldScaleDownJetISP();
+
+        bool isVolatileSource();
+
+        float getMinimumThermalPower();
+
+        float getCoreTempAtRadiatorTemp(float rad_temp);
+
+        float getThermalPowerAtTemp(float temp);
+
+	}
+}
+
